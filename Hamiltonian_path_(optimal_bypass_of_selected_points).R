0a3792e47bbf2cc4bee00782dd3d1201f589c4fe
Konstantin <nicemind@ya.ru>
2017-07-29 00:27:05 +0300

    new version

--- Hamiltonian_path_(optimal_bypass_of_selected_points).R
+++ Hamiltonian_path_(optimal_bypass_of_selected_points).R
@@ -26,19 +26,20 @@ grid_grapn_nrow= nrow(grid_graph)
 wsp3<-cbind(n1,wsp2)                       # dataframe with numbers of cells and dose rate 
 
 # road mask
-mask_buildings<-raster("170412good_mask_roads_inv.tif")
+mask_buildings<-raster("170412good_mask_buildings.tif")
 extent(mask_buildings)<-wsp
-mask_buildings<-resample(mask_buildings, grid_graph, method='bilinear') # the mask in same resolutioan as a grid
+mask_buildings<-resample(mask_buildings, grid_graph, method='ngb') # the mask in same resolutioan as a grid
 nrow(mask_buildings); ncol(mask_buildings)
 # Criterion of the action of the mask 1000 - visible, 0- invisible, 150 - color boundary (0-255), giving boundaries of buildings
-mask_buildings[which(mask_buildings[]>150)]=1000  
-mask_buildings[which(mask_buildings[]<150)]=0
+mask_buildings[which(mask_buildings[]>0.60)]=1000  
+mask_buildings[which(mask_buildings[]<0.60)]=0
 #plot(mask_buildings,alpha=0.7,add=TRUE,gridded=T) 
 # end of road mask
 
 #Selection of the start and end points on the radiation situation map
+numberofpoints = 4	# number of points to visit								
 par(plt=c(0,1,0,1))
-p7<-click(grid_graph,n=3,cell=TRUE,type="p",plt=c(0,1,0,1))# (n= number of points to visit)
+p7<-click(grid_graph,n=numberofpoints,cell=TRUE,type="p",plt=c(0,1,0,1))# (n= number of points to visit)
 point_1<-p7[1,1];point_2<-p7[1,1]   # The route starts and ends at the first point
 #Selection end
 
@@ -68,6 +69,36 @@ while(i < ncell(grid_graph)) {
 }
 #end of creation of a date-frame
 
+#Creation of a date-frame describing the __distance__weight__ of the edges of the graph-lattice
+# time for 1 horizontal edge= 1, for diagonal =1.4
+i=1
+df_dist=data.frame(x=double(),y=double(),dose_rate=double()) # empty dataframe for weights
+while(i < ncell(grid_graph)) {
+  if (mask_buildings[i]!=0){ # If there is a mask, then do not build an egde 
+    if (ceiling(i/grid_grapn_ncol)!=(i/grid_grapn_ncol)){          # Check for the last column
+      df_dist=rbind(df_dist,c(i,i+1,1))  
+    }
+    if (ceiling(i/grid_grapn_nrow)<grid_grapn_nrow){          # Check for the last row
+      df_dist=rbind(df_dist,c(i,i+grid_grapn_ncol,1)) 
+      if (ceiling(i/grid_grapn_ncol)!=(i/grid_grapn_ncol)){    #1 diagonal "\", Multiply by 1.4 - Longer walk diagonally
+        df_dist=rbind(df_dist,c(i,i+1+grid_grapn_ncol,1.4))  
+      }
+      if (i>1) { 
+        if (ceiling((i-1)/grid_grapn_ncol)!=((i-1)/grid_grapn_ncol)){    #2 diagonal "/", Multiply by 1.4 - Longer walk diagonally
+          df_dist=rbind(df_dist,c(i,i-1+grid_grapn_ncol,1.4))  
+        }
+      }
+    }
+    
+  }
+  
+  i=i+1
+}
+el_dist<-as.matrix(df_dist)
+g_dist<- add.edges(graph.empty(ncell(grid_graph)), t(el_dist[,1:2]), weight=el_dist[,3])
+g_dist<-as.undirected(g_dist)
+#end of creation of a distance weight date-frame
+
 #Computation of the shortest path in the graph
 el<-as.matrix(df)
 g<- add.edges(graph.empty(ncell(grid_graph)), t(el[,1:2]), weight=el[,3])
@@ -76,25 +107,44 @@ g<-as.undirected(g)
 
 # optimal route finder
 n_points<-length(p7[,1])    # number of points to visit
-dose=matrix(nrow = n_points, ncol = n_points)
-for(i in 1:(n_points-1)){
+dose=matrix(nrow = n_points, ncol = n_points)  #dose by summarizing the edge weights (More precisely, takes into account large doses on the diagonal 1.4)
+#dose2=matrix(nrow = n_points, ncol = n_points) #dose by extracting from grid (not right)
+dose_dist=matrix(nrow = n_points, ncol = n_points) #storage for distance per route
+DF_XY=data.frame(i=integer(),j=integer(),rout=integer()) #storage for routes
+for(i in 1:(n_points)){
   for(j in i:n_points){
-    routine<-get.shortest.paths(g,from=p7[i,1],to=p7[j,1],weights = NULL) # search for the route with a minimal dose
-    rout<-unlist(routine)
-    XY<-xyFromCell(grid_graph,rout)        #Obtaining coordinates of waypoints from raster cell numbers
+    if (i!=j) {
+      routine<-get.shortest.paths(g,from=p7[i,1],to=p7[j,1],weights = NULL, output = "both") # search for the route with a minimal dose
+      List_epath=unlist(routine$epath)
+      dose[i,j]<-sum(E(g)[List_epath]$weight)
+      dose_dist[i,j]<-sum(E(g_dist)[List_epath]$weight)
+      rout<-unlist(routine$vpath)
+      DF_XY=rbind(DF_XY,data.frame(p7[i,1],p7[j,1],rout))
+    } else {
+      dose[i,j]<-0
+      dose_dist[i,j]<-0
+    }
+    print(paste(i,j,dose[i,j],dose_dist[i,j]))
+    print(paste(p7[i,1],p7[j,1]))
+    print(rout)
+    print("------")
     #Calculation of the dose by the shortest route
-    dd<-grid_graph[rout]                   # obtain the values of dose rate for the optimal trajectory
-    dose[i,j]<-sum(dd)                       # find the dose along the optimal route
+    #dd<-grid_graph[rout]
+    #print (paste("dd=",dd,"sum(dd)=",sum(dd), '| sum(E(g))=',dose[i,j]))
+    # obtain the values of dose rate for the optimal trajectory
+    #dose2[i,j]<-sum(dd)                       # find the dose along the optimal route by extracting from grid
   }
 }
 #The calculation of the dose matrix for the shortest routes between the visiting points is completed
 
 #Calculation of doses for all possible Hamiltonian paths between points of visits (the first point is the beginning and the end of the path is always)
-dose[lower.tri(dose)] = t(dose)[lower.tri(dose)] # The dose matrix is symmetrized 
+dose[lower.tri(dose)] = t(dose)[lower.tri(dose)] # The dose matrix is symmetrized (!!!2017-07-27)
 Dose=dose
+dose_dist[lower.tri(dose_dist)] = t(dose_dist)[lower.tri(dose_dist)] # The distance matrix is symmetrized
 g1 <- graph.ring(n_points)   # Random permutation of a random graph g1
-kk<-factorial(n_points)     # kk is chosen such that the smallest dose appears in the cycle several times
-D_h<-rep(0,kk);             # A vector is created to store the doses
+kk<-factorial(n_points)     
+D_h<-rep(0,kk)            # A vector is created to store the doses
+D_h_dist<-rep(0,kk)       # A vector is created to store the distance on every step in the grid
 rr<-matrix(nrow=kk,ncol=n_points,c(0:0)) # A matrix is created for the saving of the order of bypass of selected Points
 ss<-matrix(nrow=kk,ncol=n_points,c(0:0)) # A matrix is created for storing the "sample"
 for (j in 1:kk){ #A cycle of calculation of doses is started on randomly chosen Hamiltonian paths
@@ -104,15 +154,20 @@ for (j in 1:kk){ #A cycle of calculation of doses is started on randomly chosen 
   el2<-as.matrix(Dosemat)
   dl<-rep(0,n_points)
   el2<-cbind(el2,dl) #The creation of a matrix of weights of one of the possible Hamiltonian graphs
-  for (i in 1:n_points) {el2[i,3]<-Dose[el2[i,1],el2[i,2]]}
+  el2_dist<-el2 #for total time on route calculation
+  for (i in 1:n_points) {
+    el2[i,3]<-Dose[el2[i,1],el2[i,2]]
+    el2_dist[i,3]<-dose_dist[el2[i,1],el2[i,2]]
+  }
   D<-colSums(el2)   #The dose value is calculated from the current Hamiltonian cycle
   D_h[j]<-D[3]
+  D_h_dist[j]<-colSums(el2_dist)[3] #sum of steps on the route												  
   ss[j,]<-smp
   rr[j,]<-el2[,1] #The dose along the Hamiltonian route and the route itself are stored
 }
 max(D_h);min(D_h)# Determined min and max dose over all Hamiltonian bypass route
-NN<-which(D_h==min(D_h))
-
+NN<-which(D_h==min(D_h))[1] # first variant with minimum dose
+total_dist=D_h_dist[NN]
 #Construction of the route of detour of selected visiting points
 r_beg<-ss[NN[1],]
 r_end<-rep(0,n_points)
@@ -122,26 +177,38 @@ r_end[n_points]<-r_beg[1]
 pairs<-cbind(r_beg,r_end)#A matrix of transitions between the vertices of the Hamiltonian graph with min dose
 
 for(i in 1:n_points){
-  routine<-get.shortest.paths(g,from=p7[pairs[i,1],1],to=p7[pairs[i,2],1],weights = NULL) # Find the path with the lowest dose rate
-  rout<-unlist(routine)
+  print(paste(p7[pairs[i,1],1],"->",p7[pairs[i,2],1]))
+  pointS1=p7[pairs[i,1],1]
+  pointS2=p7[pairs[i,2],1]
+  rout=DF_XY[which((DF_XY[,2]==pointS2) &(DF_XY[,1]==pointS1)),3] 
+  # if it is no 1-2, then we mirror it for 2-1.
+  if (length(rout)==0) {
+    rout=DF_XY[which((DF_XY[,2]==pointS1) &(DF_XY[,1]==pointS2)),3]
+  }
   XY<-xyFromCell(grid_graph,rout)        #Obtaining coordinates of waypoints from raster cell numbers
   par(plt=c(0,1,0,1))
-  lines(XY,col=linecolors[i],lwd=3)              #the route with the lowest dose on map 
+  lines(XY,col=linecolors[i],lwd=3)  
 }
 
 #dose on route
-# The dose in the weigths was in μSv/h, reworked for the size of the map, calculate the size of the map in meters
+# The dose in the weigths was in mcSv/h, reworked for the size of the map, calculate the size of the map in meters
 xdist=distm (c(ymin(wsp), xmin(wsp)), c(ymax(wsp), xmin(wsp)), fun = distHaversine)
 ydist=distm (c(ymin(wsp), xmin(wsp)), c(ymin(wsp), xmax(wsp)), fun = distHaversine)
 stepx=xdist/Ncol
-stepy=ydist/Nrow
+#ydist=ymax(wsp) -ymin(wsp) # Step in the horizontal and vertical are equal (specified in interpolation.R)
+#stepy=ydist/Nrow
+# total_dist means sum of all grid cells on the route, where 1 = dist for passing horizontal and 
+#vertical cells, 1.4 - for diagonal.
+# if the distance within one cell is "stepx" value (for square grid) and walkspeed is
 walkspeed=1
-# Let the speed of person  = 1 m/s, then the time of transition along the edge of the graph = step/1
-# Step in the horizontal and vertical are equal (specified in interpolation.R)
+# then the distance of transition throw all cell (in meters) =total_dist*stepx
+Total_dist_sized = total_dist*stepx
+# then the time for all route is [in seconds]
+Total_time=Total_dist_sized/walkspeed
 # Dose is =
 walktimestep=(stepx/walkspeed)/3600 # in hours
-Dose=min(D_h) * walktimestep
+Total_Dose=min(D_h) * walktimestep
 #Time on the route is conditional, because the diagonal elements are also considered to pass for the "walktimestep"
-paste('The dose on the route after visiting all the given points =',round(Dose,4),'mcSv') 
+paste('The dose on the route after visiting all the given points =',round(Total_Dose,2),'mcSv', 'route length=',round(Total_dist_sized,2),'m','time on route=',round(Total_time/60,2),'minutes, speed=',walkspeed,'m/s') 
 # Time of calculation
 proc.time() - ptm
